12a56e18626523d5d0f591c40603e8fcf88e1f48
knjnjb25 <166186739+knjnjb25@users.noreply.github.com>
2024-04-14 11:36:35 +0200

    Update منشن.js

--- plugins/منشن.js
+++ plugins/منشن.js
@@ -1,16 +1,19 @@
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}//غير ڤينوم و حط اسم بوتك
-let pesan = args.join` `
-let oi = `*⬤🦂╎ جـروب : *○'${groupname}'*`
-let oi = `⬤🦂╎ الاعـضـاء : *○'${membercount}'*`
-let teks = `*منش👾 🌸 جماعي* \n\n ${oi}\n\n*🌿┇الجروب :⇣*\n`
-for (let mem of participants) {
-teks += `*◯🍒╎* @${mem.id.split('@')[0]}\n`}
-teks += `*𝑯𝑼 𝑻𝑨𝑶_𝑩𝑶𝑻*\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+    if (!(isAdmin || isOwner)) {
+        global.dfail('admin', m, conn)
+        throw false
+    }
+    // تعريف المتغيرات بالشكل الصحيح
+    let groupname = conn.getName(m.chat)
+    let membercount = participants.length
+    let oi1 = `*⬤🦂╎ جـروب : *○'${groupname}'*`
+    let oi2 = `⬤🦂╎ الاعـضـاء : *○'${membercount}'*`
+    let teks = `*منش👾 🌸 جماعي* \n\n ${oi1}\n${oi2}\n\n*🌿┇الجروب :⇣*\n`
+    for (let mem of participants) {
+        teks += `*◯🍒╎* @${mem.id.split('@')[0]}\n`
+    }
+    teks += `*𝑯𝑼 𝑻𝑨𝑶_𝑩𝑶𝑻*\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+    conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) })
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
