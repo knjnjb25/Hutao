e7d3f0d682906bc4bb5b2b27d80a1727ebf1620e
knjnjb25 <166186739+knjnjb25@users.noreply.github.com>
2024-04-14 08:41:01 +0200

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,22 +1,19 @@
-import fetch from "node-fetch"
+const fetch = require("node-fetch");
+
 let handler = async (m, { conn }) => {
+  let data = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/%D8%B7%D9%82%D9%85%20%D8%AD%D8%A8.json')).json();
+  let cita = data[Math.floor(Math.random() * data.length)];
 
-  //قناتي هتفيدك
-//https://whatsapp.com/channel/0029VaQ12JyLY6d1PdN5r93a
-//رقمي 967733707084
+  let cowi = await(await fetch(cita.cowo)).buffer();
+  await conn.sendFile(m.chat, cowi, '', 'ا╰━⧠━──━⧈⇓《🌸𝐇𝐔 𝐓𝐀𝐎🌸》⇓⧈━─━⧠', m);
 
+  let ciwi = await(await fetch(cita.cewe)).buffer();
+  await conn.sendFile(m.chat, ciwi, '', 'ا╰━⧠━──━⧈⇓《🌸𝐇𝐔 𝐓𝐀𝐎🌸》⇓⧈━─━⧠', m);
+};
 
-  let data = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/%D8%B7%D9%82%D9%85%20%D8%AD%D8%A8.json')).json()
-  let cita = data[Math.floor(Math.random() * data.length)]
+handler.help = ['Miku bot'];
+handler.tags = ['Miku bot'];
+handler.command = /^طقم|تطقيم$/i;
+handler.limit = true;
 
-  let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', 'ا╰━⧠━──━⧈⇓《🌸𝐇𝐔 𝐓𝐀𝐎🌸》⇓⧈━─━⧠', m)
-  let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', 'ا╰━⧠━──━⧈⇓《🌸𝐇𝐔 𝐓𝐀𝐎🌸》⇓⧈━─━⧠', m)
-}
-handler.help = ['Miku bot']
-handler.tags = ['Miku bot']
-handler.command = /^طقم|تطقيم$/i
-handler.limit = true
-
-export default handle
+module.exports = handler;
